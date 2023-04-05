08b23bbf1b407d0ae2f41837531ea89f1603ea86
polin <1144560553@qq.com>
2023-04-05 17:39:16 +0800

    v2.0.0

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -4,46 +4,35 @@
 
 using func = void *(*)(void *);
 
-BlockingQueue<pthread_t *> blockingQueue;
 pthread_t th1;
+std::atomic<int> counter(0);
 
-volatile bool stopThread = false;
 
 void *inner_join(void *) {
-    while (true) {
-        if (stopThread) {
-            pthread_t *pthread = nullptr;
-            if (blockingQueue.pop(pthread, 1000)) {
-                pthread_join(*pthread, nullptr);
-                delete pthread;
-            } else {
-                if (stopThread) {
-                    break;
-                }
-            }
-        } else {
-            pthread_t *pthread = nullptr;
-            if (blockingQueue.pop(pthread, 1000)) {
-                pthread_join(*pthread, nullptr);
-                delete pthread;
-            }
-        }
+    int sz = counter;
+    ::printf("wait %d thread to exit\n", sz);
+
+    while ((sz = counter) > 0) {
+        ::printf("wait %d thread to exit\n", sz);
+        usleep(1000000);
     }
-    ::printf("thread join exit\n");
+
     return nullptr;
 }
 
 void threadinit() {
-    pthread_create(&th1, nullptr, inner_join, nullptr);
+//    pthread_create(&th1, nullptr, inner_join, nullptr);
 }
 
 void threadstopAndjoin() {
-    stopThread = true;
-    pthread_join(th1, nullptr);
+//    pthread_join(th1, nullptr);
+    inner_join(nullptr);
 }
 
 // simple now
 void submit(func f, void *args) {
+    counter++;
+
     struct Node {
         func f;
         void *args;
@@ -58,11 +47,12 @@ void submit(func f, void *args) {
         Node *node = (Node *) nodevoid;
         auto res = node->f(node->args);
         delete node;
-//        pthread_detach(pthread_self());
+        pthread_detach(pthread_self());
+        counter--;
         return res;
     };
 
-    auto *newthread = new pthread_t();
-    pthread_create(newthread, nullptr, proxyf, (void *) node);
-    blockingQueue.push(newthread);
+//    auto *newthread = new pthread_t();
+    pthread_t newthread;
+    pthread_create(&newthread, nullptr, proxyf, (void *) node);
 }
